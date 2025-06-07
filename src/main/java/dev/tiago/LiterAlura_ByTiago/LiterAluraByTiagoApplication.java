2f468f0bad060235f6cd51998a26375d92b80657
tiagrc <tiago.rodrigues.tr99@hotmail.com>
2025-06-06 23:03:29 -0300

    iniciando

---
+++ src/main/java/dev/tiago/LiterAlura_ByTiago/LiterAluraByTiagoApplication.java
@@ -0,0 +1,13 @@
+package dev.tiago.LiterAlura_ByTiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraByTiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraByTiagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tiago/LiterAlura_ByTiago/LiterAluraByTiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tiago.LiterAlura_ByTiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraByTiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
